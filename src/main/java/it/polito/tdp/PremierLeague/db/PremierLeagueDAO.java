9bc7d12e3426a914dee19a04847e4e4fa2f5b1fe
angeloracina <angeloracina@mbp-di-angelo.lan>
2021-02-23 23:32:45 +0100

    sto provando a capire

--- src/main/java/it/polito/tdp/PremierLeague/db/PremierLeagueDAO.java
+++ src/main/java/it/polito/tdp/PremierLeague/db/PremierLeagueDAO.java
@@ -6,13 +6,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import it.polito.tdp.PremierLeague.model.Action;
+import it.polito.tdp.PremierLeague.model.Adiacenza;
 import it.polito.tdp.PremierLeague.model.Player;
+import it.polito.tdp.PremierLeague.model.PlayerGoals;
 
 public class PremierLeagueDAO {
 	
-	public List<Player> listAllPlayers(){
+	public List<Player> listAllPlayers(Map<Integer, Player> idMap){
 		String sql = "SELECT * FROM Players";
 		List<Player> result = new ArrayList<Player>();
 		Connection conn = DBConnect.getConnection();
@@ -23,8 +26,10 @@ public class PremierLeagueDAO {
 			while (res.next()) {
 
 				Player player = new Player(res.getInt("PlayerID"), res.getString("Name"));
-				
-				result.add(player);
+				if(!idMap.containsKey(player.getPlayerID())) {
+					result.add(player);
+					idMap.put(player.getPlayerID(), player);
+				}
 			}
 			conn.close();
 			return result;
@@ -50,6 +55,71 @@ public class PremierLeagueDAO {
 						res.getInt("Assists"),res.getInt("TotalFoulsConceded"),res.getInt("Offsides"));
 				
 				result.add(action);
+			}
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public List<Player> getVertex(Map<Integer, Player> idMap, double goals){
+		List<Player> result = new ArrayList<>();
+		String sql = "select distinct a.`PlayerID`as id, avg(a.`Goals`) as media "
+				+ "from actions as a "
+				+ "group by id"
+				+ " having media > ?";
+		
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setDouble(1, goals);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+
+				Player p = idMap.get(res.getInt("id"));
+				
+				if(p != null)
+					result.add(p);
+			}
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	public List<Adiacenza> getEdges(Map<Integer, Player> idMap){
+		List<Adiacenza> result = new ArrayList<>();
+		String sql = "select a1.`PlayerID`as id1, a2.`PlayerID` as id2, "
+				+ "sum(a1.`TimePlayed`) as sum1, sum(a2.`TimePlayed`) as sum2 "
+				+ "from actions as a1, actions as a2 "
+				+ "where a1.`PlayerID` > a2.`PlayerID` "
+				+ "and a1.`TeamID` != a2.`TeamID` "
+				+ "and a1.`MatchID` = a2.`MatchID` "
+				+ "and a1.`Starts` = 1 and a2.`Starts`=1 "
+				+ "group by id1, id2 "
+				+ "having (sum1 - sum2) != 0 ";
+		
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+
+				Player p1 = idMap.get(res.getInt("id1"));
+				Player p2 = idMap.get(res.getInt("id2"));
+				int sum1 = res.getInt("sum1");
+				int sum2 = res.getInt("sum2");
+				if(p1 != null && p2 != null)
+					result.add(new Adiacenza(p1, p2, sum1, sum2));
 			}
 			conn.close();
 			return result;
--- src/main/java/it/polito/tdp/PremierLeague/model/Model.java
+++ src/main/java/it/polito/tdp/PremierLeague/model/Model.java
@@ -1,5 +1,155 @@
 package it.polito.tdp.PremierLeague.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleDirectedWeightedGraph;
+
+import it.polito.tdp.PremierLeague.db.PremierLeagueDAO;
+
 public class Model {
 
+	private Graph<Player, DefaultWeightedEdge> grafo;
+	private PremierLeagueDAO dao ;
+	private Map<Integer, Player> idMap;
+	
+	private int titolarita;
+	private List<Player> best;
+	public Model() {
+		this.idMap = new HashMap<Integer, Player>();
+		this.dao = new PremierLeagueDAO();
+		this.dao.listAllPlayers(idMap);
+		
+	}
+	
+	public void creaGrafo(double goal) {
+		this.grafo = new SimpleDirectedWeightedGraph<>(DefaultWeightedEdge.class);
+		
+		List<Player> vertices = new ArrayList<>();
+		vertices = this.dao.getVertex(idMap, goal);
+		
+		Graphs.addAllVertices(this.grafo, vertices);
+		
+		for(Adiacenza a : this.dao.getEdges(idMap)) {
+			if(this.grafo.containsVertex(a.getP1()) && this.grafo.containsVertex(a.getP2())) {
+				if(this.grafo.getEdge(a.getP1(), a.getP2()) == null) {
+					if(a.getMin1()-a.getMin2()>0) {
+						Graphs.addEdgeWithVertices(this.grafo, a.getP1(), a.getP2(), (a.getMin1()-a.getMin2()));
+					}else {
+						Graphs.addEdgeWithVertices(this.grafo, a.getP2(), a.getP1(), (a.getMin2()-a.getMin1()));
+					}	
+				}
+			}
+		}
+	}
+	
+	public List<Player> doDreamTeam(int k) {
+		
+		this.best = new ArrayList<>();
+		titolarita = 0;
+		List<Player> parziale = new ArrayList<>();
+
+		this.ricorsiva(parziale, k, new ArrayList<Player>(this.grafo.vertexSet()));
+			
+			return best;
+	}
+	
+	private void ricorsiva(List<Player> parziale, int k, List<Player> players) {
+		// TODO Auto-generated method stub
+		if(parziale.size() == k) {
+			// stop
+			// try partial as best
+			int peso = this.getDegree(parziale);
+			if(peso > this.titolarita) {
+				best = new ArrayList<Player>(parziale);
+				titolarita = peso;
+			}
+			return;
+					}else {
+			for(Player p : players) {
+				if(!parziale.contains(p)) {
+					parziale.add(p);
+					//remove remaining 
+					List<Player> restanti = new ArrayList<Player>(players);
+					restanti.removeAll(Graphs.successorListOf(this.grafo, p));
+					ricorsiva(parziale, k, restanti);
+					//backtraching
+					parziale.remove(p);
+					
+				}
+			}
+		}
+	}
+
+	private int getDegree(List<Player> parziale) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for(Player p : parziale) {
+			sum +=(this.getPesoArchiOut(p)-this.getPesoArchiIn(p));
+		}
+		return sum;
+	}
+
+	public int getPesoArchiOut(Player p) {
+		
+		int somma = 0;
+		for(DefaultWeightedEdge e : this.grafo.outgoingEdgesOf(p)) {
+			somma += this.grafo.getEdgeWeight(e);
+		}
+		
+		return somma;
+	}
+	
+	public int getPesoArchiIn(Player p) {
+		
+		int somma = 0;
+		for(DefaultWeightedEdge e : this.grafo.incomingEdgesOf(p)) {
+			somma += this.grafo.getEdgeWeight(e);
+		}
+		return somma;
+	}
+	
+	public Player getTopPlayer() {
+		
+		if(this.grafo== null) {
+			return null;
+		}
+		int max = 0;
+		Player top = null;
+		
+		for(Player p : this.grafo.vertexSet()) {
+			if(grafo.outDegreeOf(p)> max) {
+				max = this.grafo.outDegreeOf(p);
+				top = p;
+			}
+			
+		}
+		
+		return top;
+	}
+	
+	public List<Connessione> getConnessioni(Player top){
+		
+		List<Connessione> result = new ArrayList<Connessione>();
+		
+		for(DefaultWeightedEdge e : this.grafo.outgoingEdgesOf(top)) {
+			result.add(new Connessione( this.grafo.getEdgeTarget(e), (int) this.grafo.getEdgeWeight(e)));
+		}
+		Collections.sort(result);
+		return result; 
+	}
+	
+	public int nVertices() {
+		return this.grafo.vertexSet().size();
+	}
+	
+	public int nEdges() {
+		return this.grafo.edgeSet().size();
+	}
 }
